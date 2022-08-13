79e64355f52668cfdbb36b810fb51cd1a8ab5ae2
Abhik Banerjee <abhik.banerjee.1999@gmail.com>
2022-08-13 21:05:25 +0530

    Completed adding controllers. Need to create utility for handling response and error structs.

--- crud-nosql/cmd/main/main.go
+++ crud-nosql/cmd/main/main.go
@@ -1,1 +1,22 @@
 package main
+
+import (
+	"crud-nosql/pkg/config"
+	"crud-nosql/pkg/routes"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	r := mux.NewRouter()
+	config.Connect()
+	defer config.Disconnect()
+	routes.RegisterRoutes(r)
+	fmt.Println("Starting on PORT 3000")
+	http.Handle("/", r)
+
+	log.Fatal(http.ListenAndServe(":3000", r))
+}
